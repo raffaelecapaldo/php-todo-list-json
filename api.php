15fd603228310d681d7c8ed4328c12c603c0431a
raffaelecapaldo <raffaelecpl@gmail.com>
2023-05-08 15:51:00 +0200

    Added server side controls just for be safe

--- api.php
+++ api.php
@@ -2,20 +2,20 @@
 //Leggo il file json e lo decodo
 $phpList = json_decode(file_get_contents('./data/data.json'), true);
 
-//Se c'è checkIndex settato 
-if(isset($_POST['checkIndex'])) {
+//Se c'è checkIndex settato ed è un numero
+if(isset($_POST['checkIndex']) && ctype_digit($_POST['checkIndex'])) {
     $indexToCheck = $_POST['checkIndex'];
-    $phpList[$indexToCheck]['done'] = !$phpList[$indexToCheck]['done'];
+    $phpList[$indexToCheck]['done'] = !$phpList[$indexToCheck]['done'];//Imposta il valore inverso di done a quell'indice
 }
 
-//Se c'è delete index settato
-if (isset($_POST['deleteIndex'])) {
+//Se c'è delete index settato ed è un numero
+if (isset($_POST['deleteIndex']) && ctype_digit($_POST['deleteIndex'])) {
 $indexToRemove = $_POST['deleteIndex'];
 array_splice($phpList, $indexToRemove, 1);//recuperato l'indice da POST, rimuovi l'item corrispondente dall'aray
 }
 
-//Se c'è todo settato
-if (isset($_POST['todo'])) {
+//Se c'è todo settato ed è almeno di 3 caratteri
+if (isset($_POST['todo']) && strlen($_POST['todo']) > 2) {
     $todo = $_POST['todo'];
     $done = $_POST['done'];
     $newtodo = [//Creo un nuovo array associativo con ciò che mi serve
